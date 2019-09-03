4d89224be0882b2739630274761fa6194fdaa97c
Chris Patrick <atomize101@gmail.com>
2019-09-03 18:41:00 -0400

    Connecting database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
