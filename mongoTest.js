855606f304c9d5f3afad0374e88b9c342dca3abd
Chris Patrick <atomize101@gmail.com>
2019-09-16 16:13:37 -0400

    Commenting Mongo test file for future use

--- mongoTest.js
+++ mongoTest.js
@@ -2,25 +2,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
 
+// This connects to  my Cloud Mongo DB
 mongoose.connect(keys.mongoURI);
 
+// This creates a new Schema 
 var itemSchema = new mongoose.Schema({
     name: String,
     price: Number,
 });
 
+// This creates a variable saved to the mode for my Schema
 var Item = mongoose.model('Item', itemSchema);
+
+// This will be the first item saved to the Schema
 
 var firstItem = new Item({
     name: 'Television',
     price: 200
 });
 
-firstItem.save(function(err, firstItem) {
+var secondItem = new Item({
+    name: 'Television Ally',
+    price: 100
+});
+
+// This is the process of saving the item to the database. 
+firstItem.save(function(err, item) {
     if(err) {
         console.log('Something Went wrong');
     } else {
         console.log('First Item was saved');
-        console.log(firstItem);
+        console.log(item);
+    }
+});
+
+secondItem.save(function(err, item) {
+    if(err) {
+        console.log('Something Went wrong');
+    } else {
+        console.log('Second Item was saved');
+        console.log(item);
     }
 });
