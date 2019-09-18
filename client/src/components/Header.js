56c8e62838ea3112c8e7c54829af08670e33fe67
Chris Patrick <atomize101@gmail.com>
2019-09-18 14:45:34 -0400

    Testing out Stripe API

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -12,7 +13,10 @@ class Header extends Component {
                     <li><a href="/auth/google">Login With Google</a></li>
                 );
             default: 
-                return <li><a href="api/logout">Logout</a></li>;
+            return [
+                <li key="1"><Payments /></li>,
+                <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
@@ -20,7 +24,7 @@ class Header extends Component {
         console.log(this.props);
         return (
             <nav>
-            <div className="nav-wrapper">
+            <div className="nav-wrapper teal lighten-2">
                 <Link to={this.props.auth ? '/items' : '/'}className="left brand-logo">
                     MoveMe
                 </Link>
