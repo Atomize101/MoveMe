437bdeb319671a58b19a859b261297ba9b86c8d0
Chris Patrick <atomize101@gmail.com>
2019-09-30 12:14:29 -0400

    Deploying

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,15 +8,25 @@ const Landing = () => {
                 MoveMe
             </h1>
             <h4>Moving Soon? Let us help you with that!</h4>
+            <div style={{textAlign: 'left'}} className="container">
+                <p className="ludwig">Our family had such a great experience on our transition to our new home</p>
+            </div>
+            <div style={{textAlign: 'center'}} className="container">
+                <p className="brooks">It streamlined the entire moving process for us!</p>
+            </div>
+            <div style={{textAlign: 'right'}} className="container">
+                <p className="groucho">We were able to cut our packing time in half!</p>
+            </div>
+            
             <div class="row">
                 <div class="col s12 m6">
                     <div class="card blue-grey darken-1">
                         <div class="card-content white-text">
-                            <span class="card-title">Never Forgot a thing!</span>
+                            <span class="card-title">Never Forget a thing!</span>
                             <p>MoveMe helps keep track of those items when it comes time for a new place to live!</p>
                         </div>
                         <div class="card-action">
-                            <a href="#">Click to Begin</a>
+                            <a href="/items">Click to Begin</a>
                         </div>
                     </div>
                 </div>
