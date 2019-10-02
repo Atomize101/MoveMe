e65e6c337d084cb5268fbb997812a4ccac6846de
Chris Patrick <atomize101@gmail.com>
2019-10-02 14:57:08 -0400

    Working Routes for viewing and adding items

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,26 +1,37 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-let Item = require('../models/Item');
 
 const Item = mongoose.model('items');
 
 module.exports = app => {
-    app.post('api/items', requireLogin, (req, res) => {
+    app.post('/api/items', (req, res) => {
         const name = req.body.name;
         const description = req.body.description;
         const price = req.body.price;
-        const dateAdded = Date.parse(req.body.dateAdded);
-        const _user = req.user.id;
+        //const dateAdded = Date.parse(req.body.dateAdded);
+        //const _user = req.user.id;
 
         const item = new Item({
             name,
             description,
             price,
-            dateAdded,
-            _user
+            //dateAdded
+            //_user
     });
-});
+    item.save()
+        .then(() => res.json('Item Added')
+        .catch(err => res.status(400).json('Error: ' + err)));
+    });
 }
+
+
+module.exports = app => {
+    app.get('/api/items', (req, res) => {
+        Item.find()
+        .then(items => res.json(items))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+};
 
 //const Item = mongoose.model('items');
 
